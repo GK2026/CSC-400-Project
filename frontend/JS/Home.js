05c3dfe3311eb027ebc85c01ce56d359e004d09b
Grace Kirby <gracekirby@Graces-MacBook-Air-2.local>
2026-04-01 17:03:43 -0400

    Bug Fix

--- frontend/JS/Home.js
+++ frontend/JS/Home.js
@@ -1,41 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navAuth = document.getElementById('nav-auth');
-    const first_name = sessionStorage.getItem("first_name");
     const heroTitle = document.getElementById("hero-title");
     const heroSubtext = document.querySelector(".hero-content p"); // Targets the description paragraph
-
-    // ====== Navbar & Hero Authentication Section ======
-    if (first_name) {
-        // 1. Update Navbar to show name and dropdown
-        navAuth.innerHTML = `
-            <div class="dropdown">
-                <button class="btn-solid dropdown-btn">${first_name}</button>
-                <div class="dropdown-content">
-                    <a href="#" id="logout-btn">Logout</a>
-                </div>
-            </div>
-        `;
-
-        // 2. REPLACE the Hero Header Text
-        if (heroTitle) {
-            heroTitle.innerText = `Welcome back to GapMinder Project, ${first_name}!`;
-        }
-
- 
-        // Logout logic
-        document.getElementById('logout-btn').addEventListener('click', (e) => {
-            e.preventDefault();
-            sessionStorage.clear();
-            window.location.href = "Home.html";
-        });
-
-    } else {
-        // What Guests see
-        navAuth.innerHTML = `
-            <button id="nav-signin-btn" class="btn-outline">Sign In</button>
-            <button id="nav-register-btn" class="btn-solid">Register</button>
-        `;
-
         document.getElementById('nav-signin-btn').addEventListener('click', () => {
             window.location.href = 'login.html';
         });
